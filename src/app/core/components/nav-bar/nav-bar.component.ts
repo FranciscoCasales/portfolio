5ac3ed010f1b5b9e3d9f8c9400802c0619e953bb
Jose Francisco Casales Huerta <jose.casales@softtek.com>
2020-12-09 14:49:17 -0600

    Se Ajustaron estilos en el nav-bar component

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'nav-bar',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  public isHome = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.validateRoute();
+  }
+
+  private validateRoute = () => {
+    this.router.events.pipe(
+      filter((e: Event) => e instanceof NavigationEnd)
+    ).subscribe((e: NavigationEnd) => {
+      this.isHome = e.url.endsWith('home');
+    });
   }
 
 }
