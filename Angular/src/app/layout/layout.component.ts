c2e04f6353df42d205733b85b93568a6500e7d29
Jose Francisco Casales Huerta <jose.casales@softtek.com>
2021-02-08 10:47:25 -0600

    Reestructuracion de repositorio para almacenar mas de un proyecto para el portafolio

--- Angular/src/app/layout/layout.component.ts
+++ Angular/src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routeTransitionAnimations } from '@layout/route-transition-animation';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -10,13 +11,26 @@ import { routeTransitionAnimations } from '@layout/route-transition-animation';
 })
 export class LayoutComponent implements OnInit {
 
+  private screenWidth: number;
+  private screenHeight: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getWindowSize();
   }
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animationState'];
+  }
+
+  private getWindowSize() {
+    fromEvent(window, 'resize').subscribe(event => console.log(event));
+
+    this.screenWidth = window.screen.width;
+    this.screenHeight = window.screen.height;
+    console.log('Screen width --------------->', this.screenWidth);
+    console.log('Screen height -------------->', this.screenHeight);
   }
 
 }
